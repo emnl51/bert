40a5a356f8aef8a8d7870bc0940e63ef00b11935
emnl51 <misafir1951@gmail.com>
2026-08-08 12:00:46 +0200

    Rename application to JobTrack

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
 
-    app_name: str = 'Berlin Supply Chain Tracker'
+    app_name: str = 'JobTrack'
     app_host: str = '0.0.0.0'
     app_port: int = 8080
     database_path: str = '/data/jobs.db'
