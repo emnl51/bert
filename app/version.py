a87483e52b0cc32d467e3d4c7a318300d566f623
emnl51 <misafir1951@gmail.com>
2026-09-04 21:29:41 +0200

    Format application version expression

--- app/version.py
+++ app/version.py
@@ -2,6 +2,4 @@ import os
 
 
 DEFAULT_VERSION = "17.2.2"
-VERSION = (
-    os.getenv("BERT_BUILD_VERSION") or os.getenv("APP_VERSION") or DEFAULT_VERSION
-).removeprefix("v")
+VERSION = (os.getenv("BERT_BUILD_VERSION") or os.getenv("APP_VERSION") or DEFAULT_VERSION).removeprefix("v")
